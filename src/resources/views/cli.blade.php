b741d002a128148d01b61b8073eb132a3129c833
WebPajooh <webpajooh@gmail.com>
2020-11-28 19:00:47 +0330

    Set the title

--- src/resources/views/cli.blade.php
+++ src/resources/views/cli.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>CLI Artisan Terminal/Libary</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <style>
