3195ddf7a57343fd7decdcc4cc2f1038e42c4f99
WebPajooh <webpajooh@gmail.com>
2020-11-28 18:43:25 +0330

    Some cleanup

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['namespace'=>'artisan_cli\gui\Http\Controllers', 'middleware' => config('artisan_cli.middleware')],function ()
-  {
-    Route::get(config('artisan_cli.route'),'ArtisanCliController@index');
-    Route::post('/artisan-cli-runner','ArtisanCliController@runCli');
-  });
+Route::group(['namespace' => 'artisan_cli\gui\Http\Controllers', 'middleware' => config('artisan_cli.middleware')], function () {
+    Route::get(config('artisan_cli.route'), 'ArtisanCliController@index');
+    Route::post('/artisan-cli-runner', 'ArtisanCliController@runCli');
+});
